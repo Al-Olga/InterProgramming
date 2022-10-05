f93459ef09045a7e75a908ca460e20d39b7fa9a0
Olga_A <zagaes2@rambler.ru>
2022-10-05 16:50:56 +0300

    correct programm Ex004

---
+++ Ex004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+int a;
+Console.Write("Введите число = ");
+a = Convert.ToInt32(Console.ReadLine());
+if (a%2==0)
+{
+    Console.WriteLine("число четное");
+}
+else 
+{
+    Console.WriteLine("число нечетное");
+}
