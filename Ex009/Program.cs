d8f48599f3c89c7c5e7fdd3d43e6e3ad7e7ad5ef
Olga_A <zagaes2@rambler.ru>
2022-10-05 18:46:06 +0300

    correct programm Ex009

---
+++ Ex009/Program.cs
@@ -0,0 +1,7 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+int numberA = new Random().Next(100,1000);
+Console.WriteLine(numberA);
+
+int numberB = (numberA/100)*10+(numberA%10); 
+Console.WriteLine(numberB);
