4cee32cbd69ee29079cb639307078daacd6c01aa
Olga_A <zagaes2@rambler.ru>
2022-10-05 17:36:17 +0300

    correct programm Ex007

---
+++ Ex007/Program.cs
@@ -0,0 +1,5 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int numberA = new Random().Next(100,1000);
+Console.WriteLine(numberA);
+Console.WriteLine("Вторая (с начала) цифра : {0}", numberA.ToString().ToCharArray()[1]);
