475f705e82a272e07b1f61464b4e651295941aa4
Olga_A <zagaes2@rambler.ru>
2022-10-05 17:30:47 +0300

    correct programm Ex006

---
+++ Ex006/Program.cs
@@ -0,0 +1,5 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+int numberA = new Random().Next(100,1000);
+Console.WriteLine(numberA);
+Console.WriteLine("Третья (с начала) цифра : {0}", numberA.ToString().ToCharArray()[2]);
