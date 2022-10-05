3b8c348e26c0382687c65ebe453b48f1560e66ac
Olga_A <zagaes2@rambler.ru>
2022-10-05 12:58:56 +0300

    correct programm Ex001

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+int a;
+int b;
+Console.Write("Введите первое число  А= ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число  В= ");
+b = Convert.ToInt32(Console.ReadLine());
+if (a == b*b)
+{
+    Console.WriteLine("число А является квадратом числа В");
+}
+else 
+{
+    Console.WriteLine("число А не является квадратом числа В");
+}
