10fc92eea777c4db500da3cfaeb82dd605c8811b
Olga_A <zagaes2@rambler.ru>
2022-10-05 17:11:12 +0300

    correct programm Ex005

---
+++ Ex005/Program.cs
@@ -0,0 +1,27 @@
+﻿// Показать четные числа от 1 до N
+int n;
+Console.Write("Введите количество чисел = ");
+n = Convert.ToInt32(Console.ReadLine());
+
+int i=1;
+int CountNumber=0;
+string StrNumbers;
+StrNumbers = " ";
+
+while (i<=n)
+{
+    if (i%2==0)
+    {
+        StrNumbers= StrNumbers+i+",";
+        CountNumber=CountNumber+1;
+    } 
+    i=i+1;
+}
+if (CountNumber==0)
+{
+    Console.WriteLine("Четные числа не найдены");
+}
+else
+{
+    Console.WriteLine("Четные числа: "+StrNumbers);
+}
