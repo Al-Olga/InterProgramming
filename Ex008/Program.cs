32b2940c99afa69f29281570d276193519020e45
Olga_A <zagaes2@rambler.ru>
2022-10-05 18:23:03 +0300

    correct programm Ex008

---
+++ Ex008/Program.cs
@@ -0,0 +1,28 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int numberA = new Random().Next(10,100);
+Console.WriteLine(numberA);
+
+int razr1 = numberA/10;
+int razr2 = numberA; 
+
+while (razr2>10)
+{
+    razr2=razr2-10;
+}
+if (razr2==10)
+{
+    razr2=0;
+}
+if (razr1>razr2)
+{
+    Console.WriteLine("Наибольшая цифра числа {0}: {1}", numberA, razr1);
+}
+else if (razr2>razr1)
+{
+    Console.WriteLine("Наибольшая цифра числа {0}: {1}", numberA, razr2);
+}
+else 
+{
+    Console.WriteLine("числа {0} и {1} равны ", razr1 + razr2);
+}
