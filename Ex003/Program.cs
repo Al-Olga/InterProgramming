d062e4cd94837c57ed077fd8357e8fcb7125a5ad
Olga_A <zagaes2@rambler.ru>
2022-10-05 16:39:09 +0300

    correct programm Ex003

---
+++ Ex003/Program.cs
@@ -0,0 +1,29 @@
+﻿// Найти максимальное из трех чисел
+int [] values;
+values = new int[3];
+int max;
+int i;
+
+i = 0;
+int numb=1;
+while (i<values.Length)
+{
+    Console.WriteLine("Введите число " + numb);
+    values[i] = Convert.ToInt32(Console.ReadLine());
+    i = i+1;
+    numb=numb+1;
+}
+
+i = 0;
+max = values[i];
+
+while (i<values.Length)
+{
+    if (max<values[i])
+    {
+        max = values[i];
+    }
+    i = i+1;
+}
+
+Console.WriteLine("масимальное число = "+ max);
