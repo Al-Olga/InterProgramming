caf496269cabd87c1c78fb615d50c141905a81cb
Olga_A <zagaes2@rambler.ru>
2022-10-06 10:39:25 +0300

    new-new edit programm Ex008

--- Ex008/Program.cs
+++ Ex008/Program.cs
@@ -9,15 +9,6 @@ int razr2;
 razr1 = numberA/10;
 razr2 = numberA%10; 
 
-if (razr1>razr2)
-{
-    Console.WriteLine("Наибольшая цифра числа {0}: {1}", numberA, razr1);
-}
-else if (razr2>razr1)
-{
-    Console.WriteLine("Наибольшая цифра числа {0}: {1}", numberA, razr2);
-}
-else 
-{
-    Console.WriteLine("числа {0} и {1} равны ", razr1, razr2);
-}
+if (razr1>razr2) Console.WriteLine("Наибольшая цифра числа {0}: {1}", numberA, razr1);
+if (razr2>razr1) Console.WriteLine("Наибольшая цифра числа {0}: {1}", numberA, razr2);
+if (razr2 == razr1) Console.WriteLine("числа {0} и {1} равны ", razr1, razr2);
