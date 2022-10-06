f42ad11f03c436b37d5ebbfbef070feca4978262
Olga_A <zagaes2@rambler.ru>
2022-10-06 10:27:48 +0300

    new edit programm Ex007

--- Ex007/Program.cs
+++ Ex007/Program.cs
@@ -1,20 +1,12 @@
 ﻿// Показать вторую цифру трёхзначного числа
-// без преобразований в строку/символ
 
 int numberA = new Random().Next(100,1000);
 Console.WriteLine(numberA);
 
-int rep = numberA/10;
+int rep;
 
-while (rep>10)
-{
-    rep=rep-10;
-}
-if (rep==10)
-{
-    Console.WriteLine("Вторая (с начала) цифра : " + 0);
-}
-else
-{
-    Console.WriteLine("Вторая (с начала) цифра : " + rep);
-}
+rep = numberA%100;
+rep = rep/10;
+
+if (rep == 0) Console.WriteLine("Вторая (с начала) цифра : 0");
+else Console.WriteLine("Вторая (с начала) цифра : {0}", rep);
