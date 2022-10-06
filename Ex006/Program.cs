4cf5f82496a0fb6f2ecf7443929fa4ee8d5cfe56
Olga_A <zagaes2@rambler.ru>
2022-10-06 10:19:31 +0300

    new edit programm Ex006

--- Ex006/Program.cs
+++ Ex006/Program.cs
@@ -2,4 +2,9 @@
 
 int numberA = new Random().Next(100,1000);
 Console.WriteLine(numberA);
-Console.WriteLine("Третья (с начала) цифра : {0}", numberA.ToString().ToCharArray()[2]);
+
+int rep;
+rep = numberA%10;
+
+if (rep==0) Console.WriteLine("Последняя цифра : 0");
+else Console.WriteLine("Последняя цифра : {0}", rep);
